790817d524c583e9e60d08be102c22d7b4fd0607
Aaron Soffa <asoffa@uci.edu>
2017-05-30 06:21:52 -0700

    handle multiple output directories

--- transfer_output.sh
+++ transfer_output.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-test -d ./out_mc_bkg*    && mv -i ./out_mc_bkg*    ../datasets_superNt/mc_bkg/
-test -d ./out_fakes*     && mv -i ./out_fakes*     ../datasets_superNt/fakes/
-test -d ./out_qflip*     && mv -i ./out_qflip*     ../datasets_superNt/qflip/
-test -d ./out_mc_signal* && mv -i ./out_mc_signal* ../datasets_superNt/mc_signal/
-test -d ./out_data*      && mv -i ./out_data*      ../datasets_superNt/data/
+ls ./out_mc_bkg*    1> /dev/null 2>&1  &&  mv -i ./out_mc_bkg*    ../datasets_superNt/mc_bkg/
+ls ./out_fakes*     1> /dev/null 2>&1  &&  mv -i ./out_fakes*     ../datasets_superNt/fakes/
+ls ./out_qflip*     1> /dev/null 2>&1  &&  mv -i ./out_qflip*     ../datasets_superNt/qflip/
+ls ./out_mc_signal* 1> /dev/null 2>&1  &&  mv -i ./out_mc_signal* ../datasets_superNt/mc_signal/
+ls ./out_data*      1> /dev/null 2>&1  &&  mv -i ./out_data*      ../datasets_superNt/data/
 
