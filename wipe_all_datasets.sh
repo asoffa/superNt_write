e5a1736e33aef4f8f483e54cd4f4c1d7d2f605c7
Aaron Soffa <asoffa@uci.edu>
2017-05-31 22:29:49 -0700

    add script to wipe all datasets

---
+++ wipe_all_datasets.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm -rf ../datasets_superNt/mc_bkg/out_*
+rm -rf ../datasets_superNt/fakes/out_*
+rm -rf ../datasets_superNt/qflip/out_*
+rm -rf ../datasets_superNt/mc_signal/out_*
+rm -rf ../datasets_superNt/data/out_*
+
