e5445312384269003facbf5b38932c9eb1d14dd3
Aaron Soffa <asoffa@uci.edu>
2017-06-17 08:37:30 -0700

    remove qflip/fakes run modes - qflip/fakes now processed as part of data runs

--- transfer_output.sh
+++ transfer_output.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 ls ./out_mc_bkg*    1> /dev/null 2>&1  &&  mv -i ./out_mc_bkg*    ../datasets_superNt/mc_bkg/
-ls ./out_fakes*     1> /dev/null 2>&1  &&  mv -i ./out_fakes*     ../datasets_superNt/fakes/
-ls ./out_qflip*     1> /dev/null 2>&1  &&  mv -i ./out_qflip*     ../datasets_superNt/qflip/
 ls ./out_mc_signal* 1> /dev/null 2>&1  &&  mv -i ./out_mc_signal* ../datasets_superNt/mc_signal/
 ls ./out_data*      1> /dev/null 2>&1  &&  mv -i ./out_data*      ../datasets_superNt/data/
 
--- wipe_all_datasets.sh
+++ wipe_all_datasets.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 rm -rf ../datasets_superNt/mc_bkg/out_*
-rm -rf ../datasets_superNt/fakes/out_*
-rm -rf ../datasets_superNt/qflip/out_*
 rm -rf ../datasets_superNt/mc_signal/out_*
 rm -rf ../datasets_superNt/data/out_*
 
